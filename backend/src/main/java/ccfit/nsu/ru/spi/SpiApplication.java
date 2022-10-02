052e823438c8686c2b0edfbdbab21c3b02660b0b
Худяков Даниил Александрович <d.khudyakov@trueengineering.ru>
2022-10-03 02:37:58 +0700

    update back config

--- backend/src/main/java/ccfit/nsu/ru/spi/SpiApplication.java
+++ backend/src/main/java/ccfit/nsu/ru/spi/SpiApplication.java
@@ -2,8 +2,9 @@ package ccfit.nsu.ru.spi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SpiApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/ccfit/nsu/ru/spi/configs/SecurityConfig.java
@@ -0,0 +1,25 @@
+package ccfit.nsu.ru.spi.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/api/documentation/**").permitAll()
+                .antMatchers("/**").authenticated();
+    }
+
+}
