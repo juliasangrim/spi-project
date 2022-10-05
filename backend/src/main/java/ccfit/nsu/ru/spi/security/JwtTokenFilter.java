6f2538ca3546e0f5162d2472fef38bd269efc28d
Худяков Даниил Александрович <d.khudyakov@trueengineering.ru>
2022-10-06 00:09:13 +0700

    fix filter

--- backend/src/main/java/ccfit/nsu/ru/spi/security/JwtTokenFilter.java
+++ backend/src/main/java/ccfit/nsu/ru/spi/security/JwtTokenFilter.java
@@ -19,6 +19,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         //TODO to implement
+        filterChain.doFilter(request, response);
     }
 
 }
