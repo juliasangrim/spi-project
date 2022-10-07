b154da768d4fd4a731ad9f3379383a72f116e05d
arti66x <arti66x@gmail.com>
2022-10-07 20:59:11 +0700

    hot reload + loaders

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -39,6 +39,14 @@ module.exports = {
                 test: /\.(ttf|woff|woff2)$/,
                 use: ['file-loader']
             },
+            {
+                test: /\.css$/, 
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg)$/, 
+                use: ['file-loader']
+            },
         ]
     },
 
