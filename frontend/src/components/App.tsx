c4adca9dfa3521bd2838c7eb994241408cc2799f
katya <abramushkinakatya@gmail.com>
2022-10-09 22:26:55 +0700

    Remove SignIn from App

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Navbar } from './Navbar';
 import { SignUpForm } from './SignUpForm';
-import { SignInForm } from './SignInForm';
 import '../styles/fonts.css';
 import '../styles/css-reset.css';
 import '../styles/App.css';
@@ -9,7 +8,6 @@ import '../styles/App.css';
 export const App = () => (
     <div className="app">
       <Navbar />
-      {/* <SignUpForm /> */}
-      <SignInForm />
+      <SignUpForm />
     </div>
 );
