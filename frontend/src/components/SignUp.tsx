f8a8045c7a8a9950dfaeeb8bbe9245c8097c6ff0
katya <abramushkinakatya@gmail.com>
2022-10-09 18:40:52 +0700

    Change input type for password

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -9,14 +9,13 @@ export const SignUp = () => (
             <p className='Title'>Sign up to SPI</p>
             <form className='SignUpForm'>
                 <input type="text" placeholder="Email" />
-                <input type="text" placeholder="Password"/>
-                <input type="text" placeholder="Repeat password"/>
+                <input type="password" placeholder="Password"/>
+                <input type="password" placeholder="Repeat password"/>
                 {/* TODO: input type="submit", update styles */}
                 <button>Sign up</button>
             </form>
-            <p className='Hint'>Already have an account? 
-                <a> Sign in!</a>
-            </p>
+            <pre className='Hint'>Already have an account? <a>Sign in!</a>
+            </pre>
         </div>
     </div>
 );
