5fe7644806b4cba7f2b6457df17ccbb9aec287f8
stellaris <ruslanchik.popov@gmail.com>
2022-10-10 17:00:13 +0700

    Добавлено сохранение нового пользователя в бд

--- backend/src/main/java/ccfit/nsu/ru/spi/service/AuthServiceImpl.java
+++ backend/src/main/java/ccfit/nsu/ru/spi/service/AuthServiceImpl.java
@@ -3,16 +3,22 @@ package ccfit.nsu.ru.spi.service;
 import ccfit.nsu.ru.spi.model.dto.request.AuthRequest;
 import ccfit.nsu.ru.spi.model.dto.request.RegistrationRequest;
 import ccfit.nsu.ru.spi.model.dto.response.SupportInfoResponse;
+import ccfit.nsu.ru.spi.model.entity.UserEntity;
+import ccfit.nsu.ru.spi.repository.UserRepository;
+import ccfit.nsu.ru.spi.security.UserRole;
 import com.nimbusds.oauth2.sdk.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
 
     @Override
     public TokenResponse login(AuthRequest request) {
@@ -22,7 +28,16 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public void signup(RegistrationRequest request) {
-        //TODO to implement
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalArgumentException("User already exists.");
+        }
+
+        UserEntity user = new UserEntity();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setRoles(List.of(UserRole.CLIENT));
+
+        userRepository.save(user);
     }
 
     @Override
