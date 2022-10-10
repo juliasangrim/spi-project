770a7517b39357174e8763748edd40d4ea82ea42
katya <abramushkinakatya@gmail.com>
2022-10-10 10:27:22 +0700

    Fix logo image for navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => (
     <div className="navbar">
       <div className="navbar__container">
         <div className="navbar__logo">
-          <img src="{logo}" className="navbar__logo-image" alt="logo" />
+          <img src={logo} className="navbar__logo-image" alt="logo" />
           <p>Simple Project Initializer</p>
         </div>
         <button>Sign in</button>
