e33be8988abd6a093329caee58a6ed423a95d9ea
katya <abramushkinakatya@gmail.com>
2022-10-12 00:06:00 +0700

    Fix a typo in info message

--- frontend/src/components/AlertInfo.tsx
+++ frontend/src/components/AlertInfo.tsx
@@ -6,7 +6,7 @@ export const AlertInfo = () => (
   <div className="alert alert-info">
     <p className="alert-info__message">
       Contact our support team! Email us at email@gmail.com and we'll be happy
-      to help you! help
+      to help you!
     </p>
     <img className="alert-info__cross" src={cross} alt="cross" />
   </div>
