0ce985cc55a4b6ff8cb76fe52cef58269dbbb817
Екатерина Сергеевна Абрамушкина <e.abramushkina@g.nsu.ru>
2022-10-11 17:18:58 +0000

    Markup for sign in form [feature/8127-markup-signin]

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,12 +4,13 @@ import logo from './icons/logo.svg';
 
 export const Navbar = () => (
   <div className="navbar">
-    <div className="navbar__container">
-      <div className="navbar__logo">
-        <img src={logo} className="navbar__logo-image" alt="logo" />
-        <p>Simple Project Initializer</p>
+      <div className="navbar__container">
+        <div className="navbar__logo">
+          <img className="navbar__logo-image" src={logo} alt="logo" />
+          <p>Simple Project Initializer</p>
+        </div>
+        <button>Sign up</button>
+        {/* <button>Sign in</button> */}
       </div>
-      <button>Sign in</button>
     </div>
-  </div>
 );
