18aa78d2ff7e66ded4913656b2fe1d6205b8d437
katya <abramushkinakatya@gmail.com>
2022-10-11 11:20:37 +0700

    Move icons to a separate folder

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/Navbar.css';
-import logo from './logo.svg';
+import logo from './icons/logo.svg';
 
 export const Navbar = () => (
   <div className="navbar">
--- frontend/src/components/SignUpForm.tsx
+++ frontend/src/components/SignUpForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/SignUpForm.css';
-import logo from './logo.svg';
+import logo from './icons/logo.svg';
 
 export const SignUpForm = () => (
   <div className="sign-up-form">
