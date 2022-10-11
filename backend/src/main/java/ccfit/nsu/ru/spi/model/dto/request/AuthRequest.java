e3c3b4051d6a2c8f03ef449fa68951546952206c
julia <y.trubitsyna@g.nsu.ru>
2022-10-11 09:45:44 +0700

    done validation requests

--- backend/src/main/java/ccfit/nsu/ru/spi/model/dto/request/AuthRequest.java
+++ backend/src/main/java/ccfit/nsu/ru/spi/model/dto/request/AuthRequest.java
@@ -3,12 +3,16 @@ package ccfit.nsu.ru.spi.model.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class AuthRequest {
 
+    @NotBlank(message = "Email is mandatory")
     private String email;
 
+    @NotBlank(message = "Password is mandatory")
     private String password;
 
 }
