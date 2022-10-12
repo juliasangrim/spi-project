3ae071683706193d17d7754de704840f1ec88796
Анна Александровна Котельникова <a.kotelnikova@g.nsu.ru>
2022-10-12 04:03:51 +0000

    8120-login-api

--- backend/src/main/java/ccfit/nsu/ru/spi/service/AuthServiceImpl.java
+++ backend/src/main/java/ccfit/nsu/ru/spi/service/AuthServiceImpl.java
@@ -1,17 +1,22 @@
 package ccfit.nsu.ru.spi.service;
 
+import ccfit.nsu.ru.spi.exception.NotFoundException;
 import ccfit.nsu.ru.spi.model.dto.request.AuthRequest;
 import ccfit.nsu.ru.spi.model.dto.request.RegistrationRequest;
 import ccfit.nsu.ru.spi.model.dto.response.SupportInfoResponse;
-import ccfit.nsu.ru.spi.model.entity.UserEntity;
+import ccfit.nsu.ru.spi.model.dto.response.TokenResponse;
 import ccfit.nsu.ru.spi.repository.UserRepository;
-import ccfit.nsu.ru.spi.security.UserRole;
-import com.nimbusds.oauth2.sdk.TokenResponse;
+import ccfit.nsu.ru.spi.security.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import ccfit.nsu.ru.spi.model.entity.UserEntity;
+import ccfit.nsu.ru.spi.security.UserRole;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User;
+
 
 import java.util.List;
 
@@ -22,11 +27,15 @@ public class AuthServiceImpl implements AuthService {
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenUtil jwtTokenUtil;
+
 
     @Override
     public TokenResponse login(AuthRequest request) {
-        //TODO to implement
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new NotFoundException("User doesn't exists"));
+        String token = jwtTokenUtil.generateToken(user);
+        return new TokenResponse(token);
     }
 
     @Override
--- backend/src/test/java/ccfit/nsu/ru/spi/service/AuthServiceImplTest.java
+++ backend/src/test/java/ccfit/nsu/ru/spi/service/AuthServiceImplTest.java
@@ -1,8 +1,11 @@
 package ccfit.nsu.ru.spi.service;
 
+import ccfit.nsu.ru.spi.exception.NotFoundException;
+import ccfit.nsu.ru.spi.model.dto.request.AuthRequest;
+import ccfit.nsu.ru.spi.repository.UserRepository;
+import ccfit.nsu.ru.spi.security.JwtTokenUtil;
 import ccfit.nsu.ru.spi.model.dto.request.RegistrationRequest;
 import ccfit.nsu.ru.spi.model.entity.UserEntity;
-import ccfit.nsu.ru.spi.repository.UserRepository;
 import ccfit.nsu.ru.spi.security.UserRole;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,18 +15,27 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
-class AuthServiceImplTest {
+public class AuthServiceImplTest {
 
     @Mock
-    PasswordEncoder passwordEncoder;
+    AuthenticationManager authenticationManager;
 
     @Mock
     UserRepository userRepository;
+
+    @Mock
+    JwtTokenUtil jwtTokenUtil;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
 
     @InjectMocks
     AuthServiceImpl authService;
@@ -61,5 +73,33 @@ class AuthServiceImplTest {
         Mockito.when(userRepository.existsByEmail(existedEmail)).thenReturn(true);
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> authService.signup(existedUserRegistrationRequest), "User already exists.");
+    }
+
+    @Test
+    public void rightDataLoginTest() {
+        final String userEmail = "user@test.com";
+        final String userPassword = "password";
+        final String token = "test_token";
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail(userEmail);
+        userEntity.setPassword(userPassword);
+        userEntity.setRoles(List.of(UserRole.CLIENT));
+        User user = new User(userEmail, userPassword, userEntity.getAuthorities());
+        Mockito.when(userRepository.findByEmail(userEmail)).thenReturn(Optional.of(user));
+        Mockito.when(jwtTokenUtil.generateToken(user)).thenReturn(token);
+        String testedToken = authService.login(new AuthRequest(userEmail, userPassword)).getToken();
+        Assertions.assertEquals(token, testedToken);
+    }
+
+    @Test
+    public void failedDataLoginTest() {
+        final String userEmail = "user@test.com";
+        final String userPassword = "password";
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail(userEmail);
+        userEntity.setPassword(userPassword);
+        userEntity.setRoles(List.of(UserRole.CLIENT));
+        Mockito.when(userRepository.findByEmail(userEmail)).thenThrow(NotFoundException.class);
+        Assertions.assertThrows(NotFoundException.class, () -> authService.login(new AuthRequest(userEmail, userPassword)));
     }
 }
