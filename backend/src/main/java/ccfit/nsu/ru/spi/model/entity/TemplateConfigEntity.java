d6dab3581de8163a438792055f5f67cb3cadf43c
Руслан Дмитриевич Попов <r.popov@g.nsu.ru>
2022-10-31 16:53:09 +0000

    Добавлены сущности для хранения конфигурации шаблонов и миграция

---
+++ backend/src/main/java/ccfit/nsu/ru/spi/model/entity/TemplateConfigEntity.java
@@ -0,0 +1,27 @@
+package ccfit.nsu.ru.spi.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type")
+public class TemplateConfigEntity {
+
+    @Id
+    @SequenceGenerator(name = "template_config_sequence",
+            sequenceName = "template_config_id_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "template_config_sequence")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(updatable = false, insertable = false)
+    private TemplateType type;
+
+    private ZonedDateTime lastUpdateTime;
+}
