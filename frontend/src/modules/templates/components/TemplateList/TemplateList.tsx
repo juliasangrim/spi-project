8156d960a496b831aef6c8987f3643d0d500964f
arti66x <arti66x@gmail.com>
2022-11-03 10:25:57 +0700

    layout complete

--- frontend/src/modules/templates/components/TemplateList/TemplateList.tsx
+++ frontend/src/modules/templates/components/TemplateList/TemplateList.tsx
@@ -1,13 +1,21 @@
 import React, { useState, useEffect, SyntheticEvent } from 'react';
 import { useNavigate } from "react-router-dom";
 import { getTemplates } from '../../services/GetTemplates';
+import Modal from '../../../general/components/Modal/Modal';
 
 import '../../styles/Button.css';
 import '../../styles/Table.css';
-
+interface Template { id: string, name: string, platform: string, created: string }
 function TemplateList() {
-    const [templatesList, setTemplatesList] = useState('');
+    const [modalAddActive, setModalAddState] = React.useState(false);
+    const [modalEditActive, setModalEditState] = React.useState(false);
+    const [templatesList, setTemplatesList] = useState<Template[]>([]);
     useEffect(() => {
+        let table: Template[] = []
+        for (let i = 0; i < 3; i++) {
+            table.push({ id: '1', name: 'Template #1', platform: 'Spring', created: '28.10.2022 12:00' })
+        }
+        setTemplatesList(table)
         getTemplates()
             .then((res) => {
                 console.log(res)
@@ -18,48 +26,51 @@ function TemplateList() {
     }, []);
 
     return (
-        <div className='flex-col items-center w-fit mx-auto mt-[30px] pt-[20px] w-fit max-w-full'>
-            <div className='flex justify-end items-right mx-[30px] mb-[15px]'>
-                <button className='btn btn-green'>
-                    Add
-                </button>
+        <div>
+            <div className='flex-col items-center w-fit mx-auto mt-[30px] pt-[20px] w-fit max-w-full'>
+                <div className='flex justify-end items-right mx-[30px] mb-[15px]'>
+                    <button className='btn btn-green'
+                        onClick={() => setModalAddState(true)}
+                    >
+                        Add
+                    </button>
+                </div>
+                <div className="overflow-x-auto">
+                    <table className='table-default'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Platform</th>
+                                <th>Created</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {templatesList.map(
+                                entity =>
+                                    <tr key={entity.id}>
+                                        <td>{entity.id}</td>
+                                        <td>{entity.name}</td>
+                                        <td>{entity.platform}</td>
+                                        <td>{entity.created}</td>
+                                        <td><button className='btn-reference'
+                                            onClick={() => setModalEditState(true)}
+                                        >
+                                            Edit
+                                        </button></td>
+                                    </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div className="overflow-x-auto">
-                <table className='table-default'>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Platform</th>
-                            <th>Created</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>1</th>
-                            <td>Template #1</td>
-                            <td>Spring</td>
-                            <td>28.10.2022 12:00</td>
-                            <td>Edit</td>
-                        </tr>
-                        <tr>
-                            <th>1</th>
-                            <td>Template #1</td>
-                            <td>Spring</td>
-                            <td>28.10.2022 12:00</td>
-                            <td>Edit</td>
-                        </tr>
-                        <tr>
-                            <th>1</th>
-                            <td>Template #1</td>
-                            <td>Spring</td>
-                            <td>28.10.2022 12:00</td>
-                            <td>Edit</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            <Modal active={modalAddActive} setModalState={setModalAddState}>
+                Hello
+            </Modal>
+            <Modal active={modalEditActive} setModalState={setModalEditState}>
+                Edit
+            </Modal>
         </div>
     )
 }
