d735e0fd2c197323104fe9de546bb0c485c2a388
stellaris <ruslanchik.popov@gmail.com>
2022-11-15 20:47:13 +0700

    Реализовано API для получения зависимостей и версий зависимости

---
+++ backend/src/main/java/ccfit/nsu/ru/spi/mapper/DependencyInfoMapper.java
@@ -0,0 +1,17 @@
+package ccfit.nsu.ru.spi.mapper;
+
+
+import ccfit.nsu.ru.spi.model.dto.request.dependencies.SpringDependencyDTO;
+import ccfit.nsu.ru.spi.model.dto.request.dependencies.SpringDependencyVersionResponse;
+import ccfit.nsu.ru.spi.model.dto.response.dependencies.search.mavenresponse.DependencyInfo;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface DependencyInfoMapper {
+    SpringDependencyVersionResponse mapToSpringDependencyVersion(DependencyInfo dependencyInfo);
+    @Mapping(target = "versionType", ignore = true)
+    @Mapping(source = "latestVersion", target = "version")
+    SpringDependencyDTO mapToSpringDependency(DependencyInfo dependencyInfo);
+}
