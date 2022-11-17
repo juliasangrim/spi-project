3effc4953d0a975cb6ae20df6abdb9a5000d2279
Екатерина Сергеевна Абрамушкина <e.abramushkina@g.nsu.ru>
2022-11-17 09:16:05 +0000

    Markup for editing default configuration

--- frontend/src/modules/template/components/AddTemplate.tsx
+++ frontend/src/modules/template/components/AddTemplate.tsx
@@ -1,25 +1,22 @@
-import * as React from 'react'
-import {ITemplate} from "../../../types/ApiTypes";
+import * as React from 'react';
+import { ITemplate } from '../../../types/ApiTypes';
 
-const AddTemplate = () => {
-    const [formData, setFormData] = React.useState<ITemplate | {}>();
+function AddTemplate() {
+  const [formData, setFormData] = React.useState<ITemplate | {}>();
 
-    return (
-        /*
-        * Add the UI here
-        * */
-        <>
-            <div className="sign-in-form">
-                <div className="sign-in-form__container">
-                    <p className="sign-in-form__title">Create Template</p>
-                    <form className="sign-in-form__input-container">
-                         Your UI
-                    </form>
-                </div>
-            </div>
-
-        </>
-    )
+  return (
+  /*
+   * Add the UI here
+   */
+    <div className="sign-in-form">
+      <div className="sign-in-form__container">
+        <p className="sign-in-form__title">Create Template</p>
+        <form className="sign-in-form__input-container">
+          Your UI
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default AddTemplate;
