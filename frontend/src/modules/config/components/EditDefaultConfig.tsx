7c77ba8f9c8ab64ab46598b7ab47169d38445a29
Godran95 <almega95@gmail.com>
2022-11-24 18:41:13 +1000

    remove console.log

--- frontend/src/modules/config/components/EditDefaultConfig.tsx
+++ frontend/src/modules/config/components/EditDefaultConfig.tsx
@@ -1,18 +1,19 @@
-import * as React from "react";
-import Button from "./Button";
-import ButtonCancel from "./ButtonCancel";
-import ButtonDelete from "./ButtonDelete";
-import EditParameterForm from "./EditParameterForm";
-import GetTableClickableRow from "./GetTableClickableRow";
-import GetTableHeaderRow from "./GetTableHeaderRow";
-import GetTableRow from "./GetTableRow";
-import Modal from "../../general/components/Modal/Modal";
-import "../styles/EditDefaultConfig.css";
-import "../styles/EditDefaultConfigTable.css";
-import { ApiContext } from "../../../context/ApiContext";
-import { ApiContextType, ITemplateType } from "../../../types/ApiTypes";
-import { useEffect } from "react";
-import API from "../../general/Api";
+import * as React from 'react';
+import Button from './Button';
+import ButtonCancel from './ButtonCancel';
+import ButtonDelete from './ButtonDelete';
+import EditParameterForm from './EditParameterForm';
+import GetTableClickableRow from './GetTableClickableRow';
+import GetTableHeaderRow from './GetTableHeaderRow';
+import GetTableRow from './GetTableRow';
+import Modal from '../../general/components/Modal/Modal';
+
+import '../styles/EditDefaultConfig.css';
+import '../styles/EditDefaultConfigTable.css';
+import { ApiContext } from '../../../context/ApiContext';
+import { ApiContextType } from '../../../types/ApiTypes';
+import { useEffect } from 'react';
+import API from '../../general/Api';
 
 function EditDefaultConfig() {
   const { templateConfigs, setTemplateConfigs } = React.useContext(
@@ -29,15 +30,14 @@ function EditDefaultConfig() {
 
   useEffect(() => {
     API.makeRequest({
-      endpoint: "templates/configs",
-      method: "GET",
+      endpoint: 'templates/configs',
+      method: 'GET',
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
       },
     })
       .then((response) => {
         if (response.data) setTemplateConfigs(response.data);
-        console.log(templateConfigs);
       })
       .catch((err) => {
         console.log(err);
@@ -97,19 +97,19 @@ function EditDefaultConfig() {
 
   const dependencies: Dependency[] = [
     {
-      groupId: "io.easyspring.security",
-      artId: "spring-security-authentication",
-      version: "1.1.0-RELEASE",
+      groupId: 'io.easyspring.security',
+      artId: 'spring-security-authentication',
+      version: '1.1.0-RELEASE',
     },
     {
-      groupId: "io.easyspring.security",
-      artId: "easy-spring-security",
-      version: "1.1.0-RELEASE",
+      groupId: 'io.easyspring.security',
+      artId: 'easy-spring-security',
+      version: '1.1.0-RELEASE',
     },
     {
-      groupId: "io.easyspring.security",
-      artId: "spring-security-authorize",
-      version: "2.7.5",
+      groupId: 'io.easyspring.security',
+      artId: 'spring-security-authorize',
+      version: '2.7.5',
     },
   ];
 
@@ -119,13 +119,23 @@ function EditDefaultConfig() {
   }
 
   const versions: Version[] = [
-    { version: "1.1.0-RELEASE", releaseDate: "25 Jan 2021" },
-    { version: "1.0.1-RELEASE", releaseDate: "14 Jan 2021" },
-    { version: "1.0.0-RELEASE", releaseDate: "10 Dec 2020" },
+    { version: '1.1.0-RELEASE', releaseDate: '25 Jan 2021' },
+    { version: '1.0.1-RELEASE', releaseDate: '14 Jan 2021' },
+    { version: '1.0.0-RELEASE', releaseDate: '10 Dec 2020' },
   ];
 
   const javaVersions: string[] = ["19", "17", "11", "8"];
 
+  const springVersions: string[] = [
+    '3.0.0 (SNAPSHOT)',
+    '3.0.0 (RC1)',
+    '2.7.6 (SNAPSHOT)',
+    '2.7.5',
+    '2.6.14 (SNAPSHOT)',
+    '2.6.13',
+  ];
+
+  const javaVersions: string[] = ['19', '17', '11', '8'];
   /* Заглушки для макетов: конец */
 
   const onTableRowClick = () => {
@@ -134,11 +144,11 @@ function EditDefaultConfig() {
   };
 
   return (
-    <div className="edit-default-config">
-      <div className="edit-default-config__body">
+    <div className='edit-default-config'>
+      <div className='edit-default-config__body'>
         <h2>Edit default configuration</h2>
-        <table className="edit-default-config__table">
-          <thead>{GetTableHeaderRow("Parameter", "Value", "Actions")}</thead>
+        <table className='edit-default-config__table'>
+          <thead>{GetTableHeaderRow('Parameter', 'Value', 'Actions')}</thead>
           <tbody>
             {templateConfigs.map((config) =>
               GetTableRow(
@@ -150,18 +160,13 @@ function EditDefaultConfig() {
           </tbody>
         </table>
 
-        <div className="dependency-table-title">
+        <div className='dependency-table-title'>
           <h3>Dependencies</h3>
-          {Button("Add dependencies", () => setAddDependencyModalState(true))}
+          {Button('Add dependencies', () => setAddDependencyModalState(true))}
         </div>
-        <table className="edit-default-config__table">
+        <table className='edit-default-config__table'>
           <thead>
-            {GetTableHeaderRow(
-              "GroupID",
-              "ArtifactID",
-              "Latest version",
-              "Actions"
-            )}
+            {GetTableHeaderRow('GroupID','ArtifactID','Latest version','Actions')}
           </thead>
           <tbody>
             {GetTableRow(
@@ -185,9 +190,9 @@ function EditDefaultConfig() {
           </tbody>
         </table>
 
-        <div className="edit-default-config__form-footer">
-          {ButtonCancel("Cancel", () => {})}
-          {Button("Save changes", () => {})}
+        <div className='edit-default-config__form-footer'>
+          {ButtonCancel('Cancel', () => {})}
+          {Button('Save changes', () => {})}
         </div>
       </div>
 
@@ -203,10 +208,10 @@ function EditDefaultConfig() {
       </Modal>
 
       <Modal active={javaModalActive} setModalState={setJavaModalState}>
-        <div className="edit-default-config__modal">
+        <div className='edit-default-config__modal'>
           <h3>Select Java version</h3>
           <EditParameterForm labelArr={javaVersions} />
-          {Button("Save", () => {})}
+          {Button('Save', () => {})}
         </div>
       </Modal>
 
@@ -214,22 +219,22 @@ function EditDefaultConfig() {
         active={addDependencyModalActive}
         setModalState={setAddDependencyModalState}
       >
-        <div className="edit-default-config__modal">
+        <div className='edit-default-config__modal'>
           <h3>Find dependencies</h3>
           <p>Dependency name:</p>
-          <div className="edit-default-config__input-group">
+          <div className='edit-default-config__input-group'>
             {/* TODO: Убрать атрибут value, он нужен только для примера */}
             <input
-              className="edit-default-config__input"
-              placeholder="Enter dependency name..."
-              value="Spring Security"
+              className='edit-default-config__input'
+              placeholder='Enter dependency name...'
+              value='Spring Security'
             />
-            {Button("Search", () => {})}
+            {Button('Search', () => {})}
           </div>
 
-          <table className="edit-default-config__table">
+          <table className='edit-default-config__table'>
             <thead>
-              {GetTableHeaderRow("GroupID", "ArtifactID", "Latest version")}
+              {GetTableHeaderRow('GroupID', 'ArtifactID', 'Latest version')}
             </thead>
             <tbody>
               {/* Заглушка: каждая строка открывает один и тот же Modal,
@@ -262,20 +267,18 @@ function EditDefaultConfig() {
         active={dependencyVersionsModalActive}
         setModalState={setDependencyVersionsModalState}
       >
-        <div className="edit-default-config__modal">
+        <div className='edit-default-config__modal'>
           <h3>Find dependencies</h3>
           <p>io.easyspring.security:spring-security-authentication</p>
-
-          <table className="edit-default-config__table">
-            <thead>{GetTableHeaderRow("Version", "Release date")}</thead>
+          <table className='edit-default-config__table'>
+            <thead>{GetTableHeaderRow('Version', 'Release date')}</thead>
             <tbody>
               {GetTableRow(versions[0].version, versions[0].releaseDate)}
               {GetTableRow(versions[1].version, versions[1].releaseDate)}
               {GetTableRow(versions[2].version, versions[2].releaseDate)}
             </tbody>
           </table>
-
-          {ButtonCancel("Back", () => {
+          {ButtonCancel('Back', () => {
             setDependencyVersionsModalState(false);
           })}
         </div>
