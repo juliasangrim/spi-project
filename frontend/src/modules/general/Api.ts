d96dd7c65f592008f02d3a2d894471f980a3107c
Artem T <arti66x@gmail.com>
2022-12-22 19:11:13 +0000

    Feature/9159 template export

--- frontend/src/modules/general/Api.ts
+++ frontend/src/modules/general/Api.ts
@@ -18,6 +18,35 @@ const makeRequest = ({
     return error;
   });
 
-const API = { makeRequest };
+const downloadFileRequest = ({
+  endpoint, method, body, errorText, headers, filename,
+}: {endpoint: string, method: string, filename: string, body?: object, errorText?: string, headers?: object}) => {
+  fetch(`http://${adress}/v1/api/${endpoint}`, {
+    method,
+    body: JSON.stringify(body),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+  })
+    .then((response) => response.blob())
+    .then((data) => {
+      const downloadUrl = window.URL.createObjectURL(data);
+      const linkUrl = document.createElement('a');
+      linkUrl.download = filename;
+      linkUrl.href = downloadUrl;
+      document.body.appendChild(linkUrl);
+      linkUrl.click();
+      document.body.removeChild(linkUrl);
+      linkUrl.remove();
+    })
+    .catch((error) => {
+      console.log(`${errorText}:`);
+      console.log(error);
+      return error;
+    });
+};
+const API = { makeRequest, downloadFileRequest };
 
 export default API;
--- frontend/src/modules/template/components/EditTemplate/ExportForm.tsx
+++ frontend/src/modules/template/components/EditTemplate/ExportForm.tsx
@@ -1,46 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../../general/components/Button/Button';
+import API from '../../../general/Api';
 
-function ExportForm() {
+interface Props {
+  templateId: number;
+  templateType: string;
+}
+
+function ExportForm({
+  templateId, templateType,
+}: Props) {
+  const [projectName, setProjectName] = useState('');
+  const [packageName, setPackageName] = useState('');
+  const [applicationName, setApplicationName] = useState('');
+
+  const onExport = (e: any) => {
+    e.preventDefault();
+
+    API.downloadFileRequest({
+      endpoint: `templates/${templateId}/export`,
+      filename: 'template.zip',
+      method: 'POST',
+      body: {
+        id: templateId,
+        type: templateType,
+        projectName: projectName || 'test-project',
+        applicationName: applicationName || 'Test',
+        packageName: packageName || 'ccfit.nsu.ru',
+      },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+      },
+    });
+  };
+
   return (
-    <form>
-      <label className="edit-template__label" htmlFor="project-name-label">
-        Project name:
-        <input
-          className="edit-template__input"
-          type="text"
-          id="project-name-label"
-          name="export-label"
-          onChange={() => {}}
-          placeholder="Enter project name..."
-        />
-      </label>
-      <label className="edit-template__label" htmlFor="name-prefix-label">
-        Package name:
-        <input
-          className="edit-template__input"
-          type="text"
-          id="name-prefix-label"
-          name="export-label"
-          onChange={() => {}}
-          placeholder="Enter package name..."
-        />
-      </label>
-      <label className="edit-template__label" htmlFor="name-prefix-label">
-        Project Class name prefix with SpringBootApplication annotation:
-        <input
-          className="edit-template__input"
-          type="text"
-          id="name-prefix-label"
-          name="export-label"
-          onChange={() => {}}
-          placeholder="Enter prefix..."
-        />
-      </label>
-      <div className="edit-template__form-footer">
-        <Button label="Export" onClick={() => {}} />
-      </div>
-    </form>
+    <div className="edit-template__modal">
+      <h3>Export template</h3>
+      <form>
+        <label className="edit-template__label" htmlFor="project-name-label">
+          Project name:
+          <input
+            className="edit-template__input"
+            type="text"
+            id="project-name-label"
+            name="export-label"
+            onChange={(e) => {
+              e.target.value = e.target.value.trim();
+              setProjectName(e.target.value);
+            }}
+            placeholder="test-project"
+          />
+        </label>
+        <label className="edit-template__label" htmlFor="name-prefix-label">
+          Package name:
+          <input
+            className="edit-template__input"
+            type="text"
+            id="name-prefix-label"
+            name="export-label"
+            onChange={(e) => {
+              e.target.value = e.target.value.trim();
+              setPackageName(e.target.value);
+            }}
+            placeholder="ccfit.nsu.ru"
+          />
+        </label>
+        <label className="edit-template__label" htmlFor="name-prefix-label">
+          Project Class name prefix with SpringBootApplication annotation:
+          <input
+            className="edit-template__input"
+            type="text"
+            id="name-prefix-label"
+            name="export-label"
+            onChange={(e) => {
+              e.target.value = e.target.value.trim();
+              setApplicationName(e.target.value);
+            }}
+            placeholder="Test"
+          />
+        </label>
+        <div className="edit-template__form-footer">
+          <Button label="Export" onClick={onExport} />
+        </div>
+      </form>
+    </div>
   );
 }
 
